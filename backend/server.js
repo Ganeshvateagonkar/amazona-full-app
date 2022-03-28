fd5cf4da15219043e63391bc0588fb1db8f9d338
Ganesh Vategaonkar <84558334+Ganeshvateagonkar@users.noreply.github.com>
2022-03-28 17:43:53 +0530

    authetication added

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,26 @@ import dotenv from "dotenv";
 import ConnectDB from "./config/db.js";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 dotenv.config();
 ConnectDB();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //sample product api
 app.use("/api/seed", seedRouter);
 
 //product routes
 app.use("/api/products", productRouter);
+//user Routes
+app.use("/api/users", userRouter);
+
+//error message
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
