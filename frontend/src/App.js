4aa38aa23c53521cb967784978376b571010ca4f
Ganesh Vategaonkar <84558334+Ganeshvateagonkar@users.noreply.github.com>
2022-03-29 00:43:23 +0530

    navbar color fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
       <div className="d-flex flex-column siteContainer">
         <ToastContainer position="bottom-center" limit={1} />
         <header>
-          <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar bg="dark" variant="dark" expand="lg" className="navbarItem">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>amazona</Navbar.Brand>
